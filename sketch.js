4223dfc45c7852cffd37d530298093e2aadb8b81
Joel Penner <113944087+Joel-Gaming@users.noreply.github.com>
2022-11-29 15:07:44 -0600

    reverted testing

--- sketch.js
+++ sketch.js
@@ -2,21 +2,12 @@
 // November 23rd 2022
 // Joel Penner
 
-
-import {Button} from './startScreen.js';
-import {buttonOne} from './startScreen.js';
-import {buttonTwo} from './startScreen.js';
-
 let state = "start";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  buttonOne = new Button(windowWidth/4, 300, 651, 75);
-  buttonTwo = new Button(windowWidth/4, 600, 651, 75);
 }
 
 function draw() {
   background(220);
-  buttonOne.display()
-  buttonTwo.display()
 }
