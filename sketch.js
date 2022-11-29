8c463e8b88166a3b9503de88905f9f7c74ef6101
Joel Penner <113944087+Joel-Gaming@users.noreply.github.com>
2022-11-29 15:05:07 -0600

    testing

--- sketch.js
+++ sketch.js
@@ -11,6 +11,8 @@ let state = "start";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  buttonOne = new Button(windowWidth/4, 300, 651, 75);
+  buttonTwo = new Button(windowWidth/4, 600, 651, 75);
 }
 
 function draw() {
